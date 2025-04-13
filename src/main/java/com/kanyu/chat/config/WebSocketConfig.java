710ef2bd1bd6311c7b1e858bdc9deb71f40fd840
enjoykanyu <creatkanyu@outlook.com>
2025-04-13 13:59:58 +0800

    配置群成员服务进入websocket

--- src/main/java/com/kanyu/chat/config/WebSocketConfig.java
+++ src/main/java/com/kanyu/chat/config/WebSocketConfig.java
@@ -1,13 +1,18 @@
 package com.kanyu.chat.config;
 
+import com.kanyu.chat.service.GroupMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfig {
+    @Autowired
+    private GroupMemberService groupMemberService;
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
+        WebSocketServe.setGroupMemberService(groupMemberService);
         return new ServerEndpointExporter();
     }
 }

26a953c61dadf9258ec65594c68ea88e8d853f59
enjoykanyu <creatkanyu@outlook.com>
2025-04-14 21:46:22 +0800

    新建群广播群成员

--- src/main/java/com/kanyu/chat/service/impl/GroupServiceImpl.java
+++ src/main/java/com/kanyu/chat/service/impl/GroupServiceImpl.java
@@ -1,8 +1,10 @@
 package com.kanyu.chat.service.impl;
 
 import cn.hutool.core.lang.UUID;
+import cn.hutool.json.JSONObject;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.kanyu.chat.common.Result;
+import com.kanyu.chat.config.WebSocketServe;
 import com.kanyu.chat.dto.ChatContentDto;
 import com.kanyu.chat.dto.FriendShipDto;
 import com.kanyu.chat.dto.GroupDto;
@@ -55,10 +57,23 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, Group> implements
         //设置系统消息
         message.setType(1);
         User own_user = UserHolder.getUser();
-        message.setContent(own_user.getUserName()+"新建了群聊");
+        String contnet = own_user.getUserName()+"新建了群聊";
+        message.setContent(contnet);
         //系统消息默认发送者为当前创建群聊的人
         message.setSenderId(own_user.getId());
         groupMessageService.save(message);
+        //广播群内其它成员更新消息列表
+        // 发送好友请求通知
+        //群成员新建落库
+        for (User member:dto.getMember()) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.set("receiveUserId", member.getId());
+            jsonObject.set("content", contnet);
+            jsonObject.set("type",5);//type=5则表示为新建群广播通知
+            WebSocketServe.sendMessageOnline(member.getId(),jsonObject.toString());
+        }
+
+
         return group;
     }
 

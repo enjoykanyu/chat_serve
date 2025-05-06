366e73e19e27198c4f84e5c1ee374eaf895f6fa3
enjoykanyu <creatkanyu@outlook.com>
2025-05-06 21:29:44 +0800

    feat：机器人持久存入db

--- src/main/java/com/kanyu/chat/controller/ChatBotController.java
+++ src/main/java/com/kanyu/chat/controller/ChatBotController.java
@@ -1,5 +1,8 @@
 package com.kanyu.chat.controller;
 
+import com.kanyu.chat.entity.ChatContent;
+import com.kanyu.chat.service.ChatService;
+import com.kanyu.chat.utils.UserHolder;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import jakarta.annotation.Resource;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.UUID;
 @RestController
@@ -19,6 +24,9 @@ public class ChatBotController {
     @Resource
     private OllamaChatModel model;
 
+    //引入存储消息服务
+    @Resource
+    private ChatService chatService;
     //模拟数据库存储会话和消息
     ChatMemory chatMemory = new InMemoryChatMemory();
 
@@ -32,9 +40,27 @@ public class ChatBotController {
 
     @GetMapping(value = "/streamChat", produces = "text/event-stream;charset=UTF-8")
     public Flux<String> streamChat(@RequestParam("message")String message){
-        //创建随机会话 ID
+        //创建随机会话 ID 存储用户发送对话
+        ChatContent chatContent = new ChatContent();
+        chatContent.setMessage(message);
+        chatContent.setSendUserId(UserHolder.getUser().getId());
         String sessionId = UUID.randomUUID().toString();
+        chatContent.setReceiveUserId(Long.valueOf(sessionId));
+        //存储机器人的回答消息
+
         ChatClient chatClient = ChatClient.builder(this.model).defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory, sessionId, 10)).build();
-        return chatClient.prompt(message).stream().content();
+        return chatClient.prompt(message)
+                .stream()
+                .content()
+                .flatMap(reply -> {
+                    // 保存机器人回复
+                    ChatContent botMsg = new ChatContent();
+                    botMsg.setSendUserId(Long.valueOf(sessionId));
+                    botMsg.setMessage(reply);
+                    return Mono.fromCallable(() -> {
+                        chatService.save(botMsg); // 非阻塞包装
+                        return reply;
+                    }).subscribeOn(Schedulers.boundedElastic()); // 异步执行:ml-citation{ref="6,8" data="citationList"}
+                });
     }
 }
